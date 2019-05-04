1c2660e65175814f1a6d3c1d269debf8158b6328
Fred Westling <fredrik.westling@gmail.com>
2019-05-04 14:17:05 +1000

    Added volunteer template download

--- src/ui/VolunteerView.js
+++ src/ui/VolunteerView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { Container, FormGroup, Alert, Col, Row } from 'reactstrap';
 import VolunteerInput from "../inputs/VolunteerInput";
+import { saveToFile_csv } from "../scheduling/utilities.js";
 
 export default class VolunteerView extends Component {
     constructor(props) {
@@ -12,6 +13,7 @@ export default class VolunteerView extends Component {
 
         this.toggleEditable = this.toggleEditable.bind(this);
         this.onChange = this.onChange.bind(this);
+        this.downloadTemplate = this.downloadTemplate.bind(this);
         this.deleteVol = this.deleteVol.bind(this);
     }
 
@@ -24,6 +26,21 @@ export default class VolunteerView extends Component {
 
       V[i] = v;
       this.props.onChange(V);
+    }
+
+    downloadTemplate() {
+      let content="role,names\n";
+      this.props.data.forEach(v => {
+        content += v.name + ","
+        v.staff.forEach((s,i) => {
+          if (s === "")
+            content += "Name " + (i+1) + ",";
+          else
+            content += s + ","
+        });
+        content += "\n";
+      });
+      saveToFile_csv("volunteer_template.csv",content);
     }
 
     deleteVol(idx) {
@@ -42,22 +59,27 @@ export default class VolunteerView extends Component {
             <br/>
                 <FormGroup>
                 <Row>
-                <Col sm={6}>
+                <Col sm={4}>
                   <Alert onClick={this.toggleEditable} color={this.state.editable ? "success" : "danger"}>Editable?</Alert>
                  </Col>
-                 <Col sm={6}>
+                 <Col sm={4}>
+                   <label onClick={this.downloadTemplate}>
+                       <Alert color="info">Download sample template</Alert>
+                   </label>
+                 </Col>
+                 <Col sm={4}>
                    <label>
-                       <Alert>Import...</Alert>
+                       <Alert>Import from template</Alert>
                        <input type="file" accept=".csv" hidden ref="input" onChange={this.importVols}/>
                    </label>
                  </Col>
                  </Row>
-                {this.props.data.map((vol,i) => {
+                  {this.props.data.map((vol,i) => {
 
-                          return (
-                            <VolunteerInput key={i} vol={vol} editable={this.state.editable} onDelete={() => this.deleteVol(i)} onChange={(v) => this.onChange(v,i)}/>
-                        );})}
-                        </FormGroup>
+                            return (
+                              <VolunteerInput key={i} vol={vol} editable={this.state.editable} onDelete={() => this.deleteVol(i)} onChange={(v) => this.onChange(v,i)}/>
+                          );})}
+                </FormGroup>
             </Container>
         );
     }
