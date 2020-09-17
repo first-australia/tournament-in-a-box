ddfa9c56faf4f3cbc38e9109e7bee813a910c340
Fred Westling <fredrik.westling@gmail.com>
2020-09-17 17:12:57 +1000

    Updates

---
+++ precache-manifest.9d0c3f8ca96a8255b38c7bde7ef1b643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e563fbfd2f3f09c538ac506bc64b6fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e1ea1209b02c4e8366c",
+    "url": "./static/css/2.2678467e.chunk.css"
+  },
+  {
+    "revision": "5b15345b0866317b5bd9",
+    "url": "./static/css/main.293f88d5.chunk.css"
+  },
+  {
+    "revision": "0e1ea1209b02c4e8366c",
+    "url": "./static/js/2.54e69916.chunk.js"
+  },
+  {
+    "revision": "0ef9351e1de74923582d0ac66ffd791c",
+    "url": "./static/js/2.54e69916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b15345b0866317b5bd9",
+    "url": "./static/js/main.d3f9fa5d.chunk.js"
+  },
+  {
+    "revision": "76aabd5fe53d39714f12",
+    "url": "./static/js/runtime-main.a956547d.js"
+  }
+]);
