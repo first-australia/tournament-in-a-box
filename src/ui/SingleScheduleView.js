0f4f8801459bbd43c3dc2064452a27c5656fb475
Galen Rowell <growell3@gmail.com>
2025-11-01 14:40:34 +1100

    fix - table ui was overlapping on some screens

--- src/ui/SingleScheduleView.js
+++ src/ui/SingleScheduleView.js
@@ -55,17 +55,19 @@ export default class SingleScheduleView extends React.Component {
         return (
             <div>
                 <strong>{this.props.session.name}</strong>
-                <Table className={this.props.editable ? "datagrid-custom unselectable" : "datagrid-custom"}>
-                    <tbody>
-                    {this.props.data.map((x, i) =>
-                        <tr key={i}>{x.map((y, j) =>
-                            <td onClick={() => this.select(i, j)} colSpan={y.colSpan}
-                                className={(this.state.swapee === y) ? y.className + " selected" : y.className}
-                                key={j}>{y.value}</td>)
-                        }</tr>)
-                    }
-                    </tbody>
-                </Table>
+                <div className="table-responsive">
+                    <Table className={this.props.editable ? "datagrid-custom unselectable" : "datagrid-custom"}>
+                        <tbody>
+                        {this.props.data.map((x, i) =>
+                            <tr key={i}>{x.map((y, j) =>
+                                <td onClick={() => this.select(i, j)} colSpan={y.colSpan}
+                                    className={(this.state.swapee === y) ? y.className + " selected" : y.className}
+                                    key={j}>{y.value}</td>)
+                            }</tr>)
+                        }
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         );
 
